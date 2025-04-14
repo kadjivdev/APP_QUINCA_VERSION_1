395723fdc38b3914a592597efa0b43b7d6084ee6
kadjivdev <kadjivdev@gmail.com>
2025-04-14 16:01:58 +0100

    ajout du departement de BTP via seeder

---
+++ database/seeders/AddDepartementSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AddDepartementSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('departements')->insert(
+            ['libelle' => 'Département BTP', 'created_at' => now(), 'updated_at' => now()],
+        );
+    }
+}
